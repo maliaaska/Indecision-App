38970189ca81a6047cfe707c2237a35ab1302a29
Arkadiusz <asmilewski@gmail.com>
2018-02-25 10:21:20 +0100

    setting up webpack for production

--- src/app.js
+++ src/app.js
@@ -4,5 +4,7 @@ import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
